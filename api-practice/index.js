9fb1baf8b531775f34bbef1545c14704e19e225a
Jake Langlois <langlois.j@northeastern.edu>
2025-11-03 20:31:54 -0500

    updating api challenge

--- api-practice/index.js
+++ api-practice/index.js
@@ -1,13 +1,40 @@
+// index.js
 const express = require("express");
+const path = require("path");
 const app = express();
 const port = 3000;
 
+// Middleware
+app.use(express.json());
+
 let users = [];
 
-app.get("/", (req, res) => {
-  res.send("Hello users:" + users)
+// Serve frontend.html at /frontend
+app.get("/frontend", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend.html"));
+});
+
+// GET all users
+app.get("/users", (req, res) => {
+  res.send("Hello users: " + users.join(", "));
+});
+
+// POST add a new user
+app.post("/users", (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    res.status(400).send("Missing name in req.body");
+    return;
+  }
+  users.push(name);
+});
+
+// TODO: DELETE a user (Try to recycle as much code from app.post as possible!)
+app.delete("/users", (req, res) => {
+
 });
 
 app.listen(port, () => {
   console.log(`Users app listening at http://localhost:${port}`);
+  console.log(`Frontend available at http://localhost:${port}/frontend`);
 });
